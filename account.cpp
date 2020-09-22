007ee51f870f0a0e12750f9acab0387c87513eb6
Russell <rdaswimmer@gmail.com>
2020-09-21 21:32:17 -0400

    implemented account

--- account.cpp
+++ account.cpp
@@ -1,1 +1,90 @@
+/* account.cpp, written by Russell Abernethy */
 
+#include "account.h"
+#include "inventory.h"
+#include <cstdlib>
+#include <cstdio>
+
+using namespace std;
+
+Account::Account(int iBalance, Inventory iInventory) {
+    balance = iBalance;
+    inv = iInventory;
+}
+
+Account::Account(int iBalance) {
+    balance = iBalance;
+}
+
+Account::Account(Inventory iInventory) {
+    balance = 0;
+    inv = iInventory;
+}
+
+Account::Account() {
+    balance = 0;
+}
+
+void Account::add(int value) {
+    balance += value;
+}
+
+void Account::set(int value) {
+    balance = value;
+}
+
+int Account::remove(int value) {
+    balance -= value;
+    return balance;
+}
+
+int Account::checkBalance() {
+    return balance;
+}
+
+bool Account::canBuy(Item item) {
+    return canBuy(item, 1);
+}
+
+bool Account::canBuy(Item item, int quantity) {
+    // you cannot buy a negative number of items, that's called selling.
+    if(quantity < 0)
+        return false;
+    // not enought items to sell
+    if(quantity > item.quantity)
+        return false;
+    // total cost excedes the current balance
+    if(balance < item.price * quantity)
+        return false;
+    return true;    
+}
+
+bool Account::transferIn(Account from, Item item, int quantity) {
+    // check if the item can be bought.
+    if(!canBuy(item, quantity))
+        return false;
+    // move items from one inventory to another.
+    inv.add(item, quantity);
+    remove(item.totalCost(quantity));
+    from.add(item.totalCost(quantity));
+    from.inv.remove(item, quantity);
+    return true;
+}
+
+bool Account::transferOut(Account to, Item item, int quantity) {
+    // check if the receiving account has enought money to buy the item(s).
+    if(!to.canBuy(item, quantity))
+        return false;
+    // move items from one inventory to another.
+    to.inv.add(item, quantity);
+    to.remove(item.totalCost(quantity));
+    add(item.totalCost(quantity));
+    inv.remove(item, quantity);
+    return true;
+}
+
+std::string Account::toString() {
+    std::string toReturn = "Balance: " + std::to_string(balance);
+    toReturn.append("\nInventory: " + inv.toString());
+    return toReturn;
+}
--- account.h
+++ account.h
@@ -1,9 +1,30 @@
 #ifndef HEADER_ACCOUNT
 #define HEADER_ACCOUNT 
+#include "inventory.h"
 /*
 Account stores the amount of money a trader has
 As well it provides helper functions for withdrawing and depositing
 cash between accounts
 */
 
+class Account: public Inventory {
+    private:
+        int balance;
+        Inventory inv;
+    public:
+        // constructors for different possiblities.
+        Account(int iBalance, Inventory iInventory);
+        Account(int iBalance);
+        Account(Inventory iInventory);
+        Account(); // initilizes an empty account
+        std::string toString(); // returns a string representation of the account.
+        void add(int value); // add funds to balance.
+        void set(int value); // sets the account balance to the passed double.
+        int remove(int value); // remove funds from balance.
+        int checkBalance(); // returns the current balance.
+        bool canBuy(Item item, int quantity); // returns T/F if there is enough currency to buy the desired number of items.
+        bool canBuy(Item item); // returns T/F if there is enough currency to buy one of the passed item.
+        bool transferIn(Account from, Item item, int quantity); // moves item(s) from another account to the current account if possible.
+        bool transferOut(Account dest, Item item, int quantity); // moves item(s) from current account to another account if possible.
+};
 #endif // !HEADER_ACCOUNT
