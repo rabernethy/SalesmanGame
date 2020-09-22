451fcd13519b8798990d4acc0222755facddccd7
Russell <rdaswimmer@gmail.com>
2020-09-22 15:02:45 -0400

    wrote documentation for item class.

--- item.cpp
+++ item.cpp
@@ -5,33 +5,83 @@
 #include <cstdlib>
 #include <cstdio>
 
-// Implementation of item.h
 using namespace std;
 
-Item::Item(std::string iname, int iprice, int ichance, int ifluctuation, int iquantity) {
-	name = iname;
-	price = iprice;
-	chance = ichance;
-	fluctuation = ifluctuation;
-	quantity = iquantity;
+/*
+Item(std::string iName, int iPrice, int iChance, int iFluctuation, int iQuantity):
+	desc:
+		--> creates an item object.
+		--> object constructor.
+		--> public method.
+	inputs:
+		--> std::string iName: the name of the item.
+		--> int iPrice: the price of the item.
+		--> int iChance: chance to spawn out of a hundred.
+			* each vendor rolls to spawn items.
+			* numbers over a hundred are more likely to be chosen mutliple times.
+		--> int iFluctuation: the amount a price can fluctuate by.
+		--> int iQuantity: the initial quantity of the item.
+*/
+Item::Item(std::string iName, int iPrice, int iChance, int iFluctuation, int iQuantity) {
+	name = iName;
+	price = iPrice;
+	chance = iChance;
+	fluctuation = iFluctuation;
+	quantity = iQuantity;
 	srand(time(0));
 }
 
+/*
+serialize(void):
+	desc:
+		--> creates a serialized version of the item for printing to the screen.
+		--> public method.
+*/
 std::string Item::serialize() {
-	return name + "@" + to_string(price) + "@" + to_string(chance) + "@" + to_string(fluctuation);
+	return name + " @" + to_string(price) + " @" + to_string(chance) + " @" + to_string(fluctuation);
 }
 
-// Generates the price based on the fluctuation
+/*
+genPrice(void):
+	desc: 
+		--> generates the selling price of an item based on the items base price and the fluctuation.
+		--> public method.
+	output:
+		--> returns an int that is the price for which an item is to be sold at.
+*/
 int Item::genPrice() {
 	return (rand()%2) ? (1):(-1) * (rand() % fluctuation) + price;
 }
 
+/*
+equals(Item item):
+	desc:
+		--> determines if two items are equal or not. the criteria is:
+			* the items must have the same name.
+		--> public method.
+	input:
+		--> Item item: the item to be compared agaginst.
+	outputs:
+		--> returns true if the items are the same.
+		--> returns false if the items are not the same.
+*/
 bool Item::equals(Item item) {
 	if(name != item.name)
 		return false;
 	return true;
 }
 
+/*
+totalCost(int quantity):
+	desc:
+		--> calculates the total cost of buy a quantity of item.
+		--> can be passed a quantity greater than the actual quantity of the item.
+		--> public method.
+	input:
+		--> int quantity: the quantity of item to calculate to cost of.
+	output:
+		--> returns the total cost of purchasing the desired quantity of item.
+*/
 int Item::totalCost(int quantity) {
 	return price * quantity;
 }
