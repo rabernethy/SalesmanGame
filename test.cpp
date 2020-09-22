604a76cd37a48897fd5b3f58eadb33bf9847ab18
Russell <rdaswimmer@gmail.com>
2020-09-22 18:03:55 -0400

    wrote some tests.

--- test.cpp
+++ test.cpp
@@ -21,7 +21,7 @@ int main(int argc, char ** argv) {
 	
 	// Inventory tests:
 
-	Inventory inv1;
+	Inventory inv1(2);
 	inv1.add(a, 1);
 	inv1.add(b, 1);
 
@@ -31,12 +31,14 @@ int main(int argc, char ** argv) {
 	// Account tests:
 	Account acc1(10000, inv1);
 	Account acc2(10000, inv2);
+
 	printf("\nBefore Transfer:\n");
 	printf("Account 1: \n%s\n", acc1.toString().c_str());
 	printf("Account 2: \n%s\n\n", acc2.toString().c_str());
 
-	if(acc1.transferOut(acc2, b, 1))
-		printf("here\n");
+	acc1.merge(acc2);
+	// if(acc1.transferOut(acc2, b, 1))
+	// 	printf("here\n");
 	
 	printf("\nAfter Transfer:\n");
 	printf("Account 1: \n%s\n", acc1.toString().c_str());
