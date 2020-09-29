c04393736c57aac0ea1429c894804f7c78090659
invention64 <jimmygottshall@gmail.com>
2020-09-28 20:57:30 -0400

    Added a messagebox drawable. It worked the first try, which is suspicious...

--- src/gui.cpp
+++ src/gui.cpp
@@ -1,24 +1,31 @@
 #include "constants.h"
 #include "gui.h"
 #include <string>
+#include <SFML/Graphics.hpp>
 
-MessageBox::MessageBox()
+MessageBox::MessageBox(sf::Font ifont, int nlines)
 {
-    for (u_int i=0; i<lines->size(); i++) {
-        lines[i] = "";
-    }
-}
-
-void MessageBox::draw()
-{
-    // TODO: Convert function to sfml
-    return;
+    font = ifont;
+    size = nlines;
+    lines = new std::string[size];
 }
 
 void MessageBox::write(std::string s)
 {
-    // TODO: Convert to sfml
-    return;
+    for (int i=size-1; i>0; i--) {
+        lines[i] = lines[i-1];
+    }
+    lines[0] = s;
 }
 
+void MessageBox::draw(sf::RenderTarget& target, sf::RenderStates state) const
+{
+    int fontsize = 18;
+    sf::Vector2u screenSize = target.getSize();
+    for (int i=0; i<size; i++) {
+        sf::Text text(lines[i], font, fontsize);
+        text.setPosition(0, screenSize.y - (i + 1) *fontsize - 2);
+        target.draw(text);
+    }
+}
 
